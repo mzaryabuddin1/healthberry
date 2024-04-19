11b30c713fdf13e81424c334a74065e0584fff07
Safee <safeeullah.hussainy@irisdigi.com>
2024-04-19 16:52:19 +0500

    session added

--- application/views/manage_users_view.php
+++ application/views/manage_users_view.php
@@ -14,53 +14,48 @@ $pagename = "manage_users";
 
 <!-- Main Content -->
 <div class="hk-pg-wrapper pb-0">
-    <div class="container-xxl">
-        <!-- Page Header -->
-        <div class="hk-pg-header pg-header-wth-tab pt-7">
-            <div class="d-flex">
-                <div class="d-flex flex-wrap justify-content-between flex-1">
-                    <div class="mb-lg-0 mb-2 me-8">
-                        <h1 class="pg-title">Users</h1>
-                        <p>Interact with users.</p>
+    <div class="container-fluid">
+        <!-- Form -->
+        <div class="card mt-3">
+            <div class="card-header d-flex justify-content-start">
+                <h6><i class="fa fa-table" aria-hidden="true"></i> Users</h6>
+            </div>
+            <div class="card-body">
+                <div class="alert alert-danger d-none" role="alert" id="error"></div>
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="table-responsive">
+                            <table class="table table-striped" id="datatable">
+                            <thead>
+                                <tr>
+                                    <th>Name</th>
+                                    <th>Email</th>
+                                    <th>Role</th>
+                                    <th>Status</th>
+                                    <th>Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php foreach ($data as $row) : ?>
+                                    <tr>
+                                        <td><?= $row['name'] ?></td>
+                                        <td><?= $row['email'] ?></td>
+                                        <td><?= $row['roles'] ?></td>
+                                        <td><?= $row['status'] ? 'Active' : 'Inactive' ?></td>
+                                        <td><button class="btn btn-primary">Edit</button></td>
+                                    </tr>
+                                <?php endforeach; ?>
+                            </tbody>
+                        </table>
+                        </div>
+                        
                     </div>
                 </div>
             </div>
-            <div class="d-flex justify-content-end">
-                <button class="btn btn-primary">CREATE NEW</button>
-            </div>
         </div>
-        <!-- /Page Header -->
-
-        <!-- Page Body -->
-        <div class="hk-pg-body">
-            <div class="table-responsive">
-                <table id="datatable" class="table nowrap table-striped">
-                    <thead>
-                        <tr>
-                            <th>Name</th>
-                            <th>Email</th>
-                            <th>Role</th>
-                            <th>Status</th>
-                            <th>Action</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php foreach ($data as $row) : ?>
-                            <tr>
-                                <td><?= $row['name'] ?></td>
-                                <td><?= $row['email'] ?></td>
-                                <td><?= $row['roles'] ?></td>
-                                <td><?= $row['status'] ? 'Active' : 'Inactive' ?></td>
-                                <td><button class="btn btn-primary">Edit</button></td>
-                            </tr>
-                        <?php endforeach; ?>
-                    </tbody>
-                </table>
-            </div>
-        </div>
-        <!-- /Page Body -->
+        <!-- /Form -->
     </div>
-
+   
 
 
 </div>
@@ -69,16 +64,3 @@ $pagename = "manage_users";
 <?php require_once('common/footer.php') ?>
 
 
-<link href="https://cdn.datatables.net/v/bs5/jszip-3.10.1/dt-2.0.1/b-3.0.0/b-colvis-3.0.0/b-html5-3.0.0/b-print-3.0.0/datatables.min.css" rel="stylesheet">
-<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/pdfmake.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/vfs_fonts.js"></script>
-<script src="https://cdn.datatables.net/v/bs5/jszip-3.10.1/dt-2.0.1/b-3.0.0/b-colvis-3.0.0/b-html5-3.0.0/b-print-3.0.0/datatables.min.js"></script>
-
-<script>
-    $("#datatable").DataTable({
-        "dom": 'Bfrtip',
-        "buttons": ['copy', 'csv', 'excel', 'print'],
-        "order": [1, 'asc'],
-        "searching": true
-    })
-</script>
